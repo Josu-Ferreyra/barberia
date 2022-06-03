491b89e9b12c948fc78da84db8e8edc1e0cd4514
Josu <josueferreyra8824@gmail.com>
2022-06-03 11:46:10 -0300

    v 0.0.4

--- build/src/js/app.js
+++ build/src/js/app.js
@@ -7,7 +7,9 @@ async function getServicios(){
     const data = await response.json();
     console.log(data);
     mostarServicios(data);
+    seleccionarServicio();
 }
+
 function mostarServicios(servicios){
     const root = document.querySelector('#catalogo');
     servicios.forEach(servicio => {
@@ -18,7 +20,7 @@ function mostarServicios(servicios){
         contenedorServicio.classList.add('servicio');
         nombreServicio.textContent = nombre;
         nombreServicio.classList.add('nombreServicio');
-        precioServicio.textContent = precio;
+        precioServicio.textContent = `$ ${precio}`;
         precioServicio.classList.add('precioServicio');
         contenedorServicio.appendChild(nombreServicio);
         contenedorServicio.appendChild(precioServicio);
@@ -26,3 +28,41 @@ function mostarServicios(servicios){
     });
 }
 
+let serviciosElegidos = [];
+function seleccionarServicio(){
+    const listaServicios = document.querySelectorAll('.servicio');
+    listaServicios.forEach(servicio => {
+        servicio.addEventListener('click', () => {
+            if(servicio.classList.contains('selected')){
+                // LE RETIRO LA CLASE 'SELECTED' PARA LOS ESTILOS
+                servicio.classList.remove('selected');
+                // LA ELIMINO DEL ARRAY DE SERVICIOS ELEGIDOS
+                serviciosElegidos = serviciosElegidos.filter(e => {
+                    return (e[0] !== servicio.textContent.split('$ ')[0]);
+                });
+            } else {
+                // LE AGREGO LA CLASE 'SELECTED' PARA LOS ESTILOS
+                servicio.classList.add('selected');
+                // LO AGREGO AL ARRAY DE SERVICIOS ELEGIDOS
+                serviciosElegidos.unshift(servicio.textContent.split('$ '));
+            }
+            // BOTON QUE MUESTRA EL TOTAL
+            const btn = document.querySelector('.btn-confirmarServicio');
+            if(serviciosElegidos.length == 0){
+                btn.classList.add('btn-apagado');
+            } else {
+                btn.classList.remove('btn-apagado');
+                calcTotal();
+                btn.textContent = `Total: $${total}`;
+            }
+        });
+    });
+}
+
+let total = 0;
+function calcTotal(){
+    const precios = serviciosElegidos.map(elemento => {
+        return parseInt(elemento[1]);
+    })
+    total = precios.reduce((x,y) => x+y );
+}
